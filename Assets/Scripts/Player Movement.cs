ad5e85079b8d9c4842cc89e7630bb29be7d03d0d
jtothec67 <jamescomer03@gmail.com>
2025-03-02 20:20:19 +0000

    Climbing
    You can climb

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -20,12 +20,14 @@ public class PlayerMovement : MonoBehaviour
 
     */
 
-
     public float groundSpeed = 5f;
     public bool isGrounded = false;
     public bool isLookingRight = true;
     public float lookRightAngle = 230;
     public float lookLeftAngle = 130;
+
+    public bool isClimbing = false;
+    public Vector3 climbPosition;
 
     public float maxHoldTime = 2f;
     public float maxJumpForce = 10f;
@@ -34,7 +36,15 @@ public class PlayerMovement : MonoBehaviour
     public bool canJump = true;
     public bool isChargingJump = false;
 
-    public Rigidbody rigidBody;
+    public Rigidbody rigidbody;
+
+    public Transform groundCheck1;
+    public Transform groundCheck2;
+    public Transform groundCheck3;
+    public Transform groundCheck4;
+
+    public Transform climbCheck1;
+    public Transform climbCheck2;
 
     // Start is called before the first frame update
     void Start()
@@ -48,14 +58,34 @@ public class PlayerMovement : MonoBehaviour
         // Shoot ray down to check if player is grounded,
         // if ray hits gameobject with tag "Ground" then player is grounded.
         // Maximum distance of ray is 1.1f (currently).
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.1f))
+        float rayDistance = 0.22f;
+        Vector3 rayDirection = Vector3.down;
+
+        RaycastHit ground1Hit;
+        Vector3 ground1RayOrigin = groundCheck1.transform.position;
+        bool grounded1 = Physics.Raycast(ground1RayOrigin, rayDirection, out ground1Hit, rayDistance);
+        Debug.DrawRay(ground1RayOrigin, rayDirection * rayDistance, Color.red);
+
+
+        RaycastHit ground2Hit;
+        Vector3 gorund2RayOrigin = groundCheck2.transform.position;
+        bool grounded2 = Physics.Raycast(gorund2RayOrigin, rayDirection, out ground2Hit, rayDistance);
+        Debug.DrawRay(gorund2RayOrigin, rayDirection * rayDistance, Color.red);
+
+        RaycastHit ground3Hit;
+        Vector3 ground3RayOrigin = groundCheck3.transform.position;
+        bool grounded3 = Physics.Raycast(ground3RayOrigin, rayDirection, out ground3Hit, rayDistance);
+        Debug.DrawRay(ground3RayOrigin, rayDirection * rayDistance, Color.red);
+
+        RaycastHit ground4Hit;
+        Vector3 ground4RayOrigin = groundCheck4.transform.position;
+        bool grounded4 = Physics.Raycast(ground4RayOrigin, rayDirection, out ground4Hit, rayDistance);
+        Debug.DrawRay(ground4RayOrigin, rayDirection * rayDistance, Color.red);
+
+        if (grounded1 && ground1Hit.collider.tag == "Ground" || grounded2 && ground2Hit.collider.tag == "Ground" || grounded3 && ground3Hit.collider.tag == "Ground" || grounded4 && ground4Hit.collider.tag == "Ground")
         {
-            if (hit.collider.tag == "Ground")
-            {
-                isGrounded = true;
-                canJump = true;
-            }
+            isGrounded = true;
+            canJump = true;
         }
         else
         {
@@ -63,14 +93,60 @@ public class PlayerMovement : MonoBehaviour
             canJump = false;
         }
 
+        if (isClimbing)
+        {
+            transform.position = climbPosition;
+            rigidbody.velocity = new Vector3(0, 0, 0);
+            canJump = true;
+        }
+
         if (isGrounded)
         {
-            rigidBody.velocity = new Vector3(0, 0, 0);
+            rigidbody.velocity = new Vector3(0, 0, 0);
         }
 
         if (isGrounded && !isChargingJump)
         {
             GroundMovement();
+        }
+        else if (isClimbing && !isChargingJump)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                isLookingRight = false;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                isLookingRight = true;
+            }
+        }
+
+            float climbRayDistance = 2f;
+        Vector3 climbRayDirection = new Vector3(0, 0, 1);
+
+        RaycastHit climb1Hit;
+        Vector3 climb1RayOrigin = climbCheck1.transform.position;
+        bool climb1 = Physics.Raycast(climb1RayOrigin, climbRayDirection, out climb1Hit, climbRayDistance);
+        Debug.DrawRay(climb1RayOrigin, climbRayDirection * climbRayDistance, Color.red);
+
+        RaycastHit climb2Hit;
+        Vector3 climb2RayOrigin = climbCheck2.transform.position;
+        bool climb2 = Physics.Raycast(climb2RayOrigin, climbRayDirection, out climb2Hit, climbRayDistance);
+        Debug.DrawRay(climb2RayOrigin, climbRayDirection * climbRayDistance, Color.red);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (climb1 && climb1Hit.collider.tag == "Climbable" || climb2 && climb2Hit.collider.tag == "Climbable")
+            {
+                isClimbing = true;
+                climbPosition = transform.position;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isClimbing = false;
         }
 
         if (isLookingRight)
@@ -99,15 +175,17 @@ public class PlayerMovement : MonoBehaviour
             {
                 Vector3 angle = new Vector3(Mathf.Cos(jumpAngle * Mathf.Deg2Rad), Mathf.Sin(jumpAngle * Mathf.Deg2Rad), 0).normalized;
                 float jumpForce = (holdTime / maxHoldTime) * maxJumpForce;
-                rigidBody.AddForce(angle * jumpForce, ForceMode.Impulse);
+                rigidbody.AddForce(angle * jumpForce, ForceMode.Impulse);
                 holdTime = 0f;
+                isClimbing = false;
             }
             else
             {
                 Vector3 angle = new Vector3(Mathf.Cos(((90 - jumpAngle) + 90) * Mathf.Deg2Rad), Mathf.Sin(((90 - jumpAngle) + 90) * Mathf.Deg2Rad), 0).normalized;
                 float jumpForce = (holdTime / maxHoldTime) * maxJumpForce;
-                rigidBody.AddForce(angle * jumpForce, ForceMode.Impulse);
+                rigidbody.AddForce(angle * jumpForce, ForceMode.Impulse);
                 holdTime = 0f;
+                isClimbing = false;
             }
 
             isChargingJump = false;
